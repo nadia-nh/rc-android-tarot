7b8c1b814189a7e608599420c3424baa25d5d227
nadia-nh <nadia.heredia.nh@gmail.com>
2026-01-23 16:20:52 -0500

    Correctly handle back button

--- app/src/main/java/com/example/simpletarot/TarotMain.kt
+++ app/src/main/java/com/example/simpletarot/TarotMain.kt
@@ -1,5 +1,6 @@
 package com.example.simpletarot
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -11,6 +12,11 @@ enum class AppScreen {
 @Composable
 fun TarotMain(viewModel: TarotViewModel) {
     val currentScreen by viewModel.currentScreen.collectAsState()
+
+    // Intercept the hardware back button
+    BackHandler(enabled = currentScreen == AppScreen.Result) {
+        viewModel.clearSpread()
+    }
 
     when (currentScreen) {
         AppScreen.Menu ->
