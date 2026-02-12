70755e43885de80df56b519374b6e89836746604
nadia-nh <nadia.heredia.nh@gmail.com>
2026-02-12 15:15:31 -0500

    Extract component of CardDisplay to their own composables
    This makes CardDisplay easier to test and to adapt to different orientations

--- app/src/main/java/com/example/simpletarot/CardDisplay.kt
+++ app/src/main/java/com/example/simpletarot/CardDisplay.kt
@@ -1,6 +1,7 @@
 package com.example.simpletarot
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,6 +26,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.simpletarot.PreviewConstants.drawnCard
 import com.example.simpletarot.data.DrawnCard
 import com.example.simpletarot.data.TarotCard
 import com.example.simpletarot.data.getMeaning
@@ -34,7 +36,7 @@ import com.example.simpletarot.ui.theme.LocalSpacing
 @Composable
 fun CardDisplay(
     drawnCard: DrawnCard,
-    onReveal: () -> Unit) {
+    onReveal: () -> Unit = {}) {
     val spacing = LocalSpacing.current
 
     Column(
@@ -43,55 +45,106 @@ fun CardDisplay(
             .padding(spacing.small),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Card(
-            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
-            shape = MaterialTheme.shapes.medium,
-            colors = CardDefaults.cardColors(
-                containerColor = MaterialTheme.colorScheme.surface
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-                .clickable { onReveal() }
-        ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center,
-            ) {
-                if (!drawnCard.isRevealed) {
-                    Icon(
-                        imageVector = Icons.Default.Star,
-                        contentDescription = "Hidden",
-                        tint = MaterialTheme.colorScheme.secondary.copy(alpha = 0.8f),
-                        modifier = Modifier.size(48.dp)
-                    )
-                } else {
-                    Log.d("tarot", "CardDisplay: ${drawnCard.card.name}")
-                    CardImage(drawnCard)
-                }
-            }
-        }
-
+        CardWithImageOnReveal(drawnCard = drawnCard, onReveal = onReveal)
         Spacer(modifier = Modifier.height(spacing.small))
+        CardMeaning(drawnCard = drawnCard)
+    }
+}
 
-        if (drawnCard.isRevealed) {
-            Text(text = drawnCard.getMeaning(),
-                color = MaterialTheme.colorScheme.onBackground,
-                style = MaterialTheme.typography.bodyMedium,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(top = spacing.extraSmall),
-                maxLines = 4,
-                overflow = TextOverflow.Ellipsis)
+@Composable
+fun CardWithImageOnReveal(
+    drawnCard: DrawnCard,
+    onReveal: () -> Unit = {}) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .clickable { onReveal() }
+    ) { CardImageOrPlaceholder(drawnCard) }
+}
+
+@Composable
+fun CardImageOrPlaceholder(drawnCard: DrawnCard) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        if (!drawnCard.isRevealed) {
+            Icon(
+                imageVector = Icons.Default.Star,
+                contentDescription = "Hidden",
+                tint = MaterialTheme.colorScheme.secondary.copy(alpha = 0.8f),
+                modifier = Modifier.size(48.dp)
+            )
+        } else {
+            Log.d("tarot", "CardDisplay: ${drawnCard.card.name}")
+            CardImage(drawnCard)
         }
+    }
+}
+@Composable
+fun CardMeaning(drawnCard: DrawnCard) {
+    if (drawnCard.isRevealed) {
+        Text(text = drawnCard.getMeaning(),
+            color = MaterialTheme.colorScheme.onBackground,
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            maxLines = 4,
+            overflow = TextOverflow.Ellipsis)
+    }
+}
+
+@Preview
+@Composable
+fun CardWithImageOnRevealPreview() {
+    Box(modifier = Modifier
+        .background(color = MaterialTheme.colorScheme.background)
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CardWithImageOnReveal(drawnCard = drawnCard)
+    }
+}
+
+@Preview
+@Composable
+fun CardImageOrPlaceholderPreview() {
+    Box(modifier = Modifier
+        .background(color = MaterialTheme.colorScheme.background)
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CardImageOrPlaceholder(drawnCard = drawnCard.copy(isRevealed = false))
+    }
+}
+
+@Preview
+@Composable
+fun CardMeaningPreview() {
+    Box(modifier = Modifier
+        .background(color = MaterialTheme.colorScheme.background)
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CardMeaning(drawnCard = drawnCard)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun CardDisplayPreview() {
-    val card = TarotCard("The Fool",
+    CardDisplay(drawnCard = drawnCard)
+}
+
+private object PreviewConstants {
+    val tarotCard = TarotCard("The Fool",
         "New beginnings, spontaneity, trust",
         "Recklessness, naivety, holding back")
         .withRankAndSuit()
-    CardDisplay(DrawnCard(card, false, isRevealed = true)) { }
+    val drawnCard = DrawnCard(tarotCard, false, isRevealed = true)
 }
