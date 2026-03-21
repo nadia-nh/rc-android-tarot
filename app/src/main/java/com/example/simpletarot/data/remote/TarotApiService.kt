ef15483184b18ae43432f7319f196abebffbd146
nadia-nh <nadia.heredia.nh@gmail.com>
2026-03-20 23:39:03 -0400

    Add TarotDto and TarotApiService for using the TarotAPI

---
+++ app/src/main/java/com/example/simpletarot/data/remote/TarotApiService.kt
@@ -0,0 +1,28 @@
+package com.example.simpletarot.data.remote
+
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.http.GET
+
+interface TarotApiService {
+    @GET("api/v1/cards/")
+    suspend fun getAllCards(): TarotApiResponse
+
+    companion object {
+        private const val BASE_URL = "https://tarotapi.dev/"
+
+        fun create(): TarotApiService {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(
+                    Json { ignoreUnknownKeys = true }
+                        .asConverterFactory(
+                            "application/json".toMediaType()
+                        ))
+                .build()
+                .create(TarotApiService::class.java)
+        }
+    }
+}
