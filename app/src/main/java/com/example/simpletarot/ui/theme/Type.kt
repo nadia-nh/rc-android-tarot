4cb95472db63fd2ff2c709b7e0c56244d18a0939
nadia-nh <nadia.heredia.nh@gmail.com>
2026-04-10 18:29:14 -0400

    Update typography

--- app/src/main/java/com/example/simpletarot/ui/theme/Type.kt
+++ app/src/main/java/com/example/simpletarot/ui/theme/Type.kt
@@ -1,37 +1,53 @@
 package com.example.simpletarot.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 val TarotTypography = Typography(
-    // Large Titles (Menu Header)
+    // Menu Headers
     headlineSmall = TextStyle(
         fontFamily = FontFamily.Serif,
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Light,  // Subtle effects
         fontSize = 24.sp,
-        letterSpacing = 2.sp
+        letterSpacing = 4.sp // Extra wide
     ),
+
     // Card Names
     titleSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily.Serif,
         fontWeight = FontWeight.Medium,
         fontSize = 18.sp,
-        lineHeight = 24.sp,
+        letterSpacing = 2.sp,
     ),
-    // Slightly Larger title
+
+    // Card Names
     titleMedium = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily.Serif,
         fontWeight = FontWeight.Medium,
         fontSize = 20.sp,
-        lineHeight = 24.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 2.sp
     ),
-    // Descriptions / Meanings
+
+    // Labels
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 11.sp,
+        letterSpacing = 1.5.sp, // Wide spacing on tiny text
+        color = Color.Gray
+    ),
+
+    // Interpretations / Meanings
     bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 14.sp,
+        fontSize = 15.sp,
+        lineHeight = 22.sp, // Increased line height for a subtle look
+        letterSpacing = 0.4.sp,
     )
 )
