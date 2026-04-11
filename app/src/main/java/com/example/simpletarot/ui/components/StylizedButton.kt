35e3ce4d312e518c7776b3541ee0449df44b4cb6
nadia-nh <nadia.heredia.nh@gmail.com>
2026-04-11 10:33:41 -0400

    Update StylizedButton to include icons

--- app/src/main/java/com/example/simpletarot/ui/components/StylizedButton.kt
+++ app/src/main/java/com/example/simpletarot/ui/components/StylizedButton.kt
@@ -1,21 +1,35 @@
 package com.example.simpletarot.ui.components
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.simpletarot.ui.theme.LocalSpacing
+import com.example.simpletarot.ui.theme.TarotSpacing
 
 @Composable
 fun StyledButton(
     text: String = "",
     style: TextStyle = MaterialTheme.typography.bodyMedium,
+    spacing: TarotSpacing = LocalSpacing.current,
+    enabled: Boolean = true,
+    displayIcon: Boolean = false,
+    icon: ImageVector? = null,
     onClick: () -> Unit = {}) {
     OutlinedButton(
+        enabled = enabled,
         onClick = onClick,
         colors = ButtonDefaults.outlinedButtonColors(
             containerColor = MaterialTheme.colorScheme.surface,
@@ -25,10 +39,17 @@ fun StyledButton(
             0.5.dp,
             MaterialTheme.colorScheme.outlineVariant)
     ) {
-        Text(
-            text,
-            style = style
-        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            if (displayIcon && icon != null) {
+                Icon(icon, contentDescription = null)
+                Spacer(Modifier.size(spacing.small))
+            }
+
+            Text(
+                text,
+                style = style
+            )
+        }
     }
 }
 
