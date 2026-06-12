4857489fbe23b45896334129f30a987583f66ef1
nadia-nh <nadia.heredia.nh@gmail.com>
2026-06-12 10:48:39 -0400

    Add getTodayToday method to DateUtils

--- app/src/main/java/com/flowworks/arcanaflux/util/DateUtils.kt
+++ app/src/main/java/com/flowworks/arcanaflux/util/DateUtils.kt
@@ -1,12 +1,21 @@
 package com.flowworks.arcanaflux.util
 
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 object DateUtils {
     fun formatTimestampDate(timestamp: Long): String {
         val format = TIMESTAMP_FORMAT_DATE
-        val sdf = java.text.SimpleDateFormat(
+        val sdf = SimpleDateFormat(
             format,
-            java.util.Locale.getDefault())
-        return sdf.format(java.util.Date(timestamp))
+            Locale.getDefault())
+        return sdf.format(Date(timestamp))
+    }
+
+    fun getTodayDate(): String {
+        return formatTimestampDate(
+            System.currentTimeMillis())
     }
 
     private const val TIMESTAMP_FORMAT_DATE = "MMM dd, yyyy"
