7396ad83101765f3974539aed0e1d5fe6feb5ce7
nadia-nh <nadia.heredia.nh@gmail.com>
2026-06-12 11:13:15 -0400

    Add separate timestamp formats for display and storage

--- app/src/main/java/com/flowworks/arcanaflux/util/DateUtils.kt
+++ app/src/main/java/com/flowworks/arcanaflux/util/DateUtils.kt
@@ -5,18 +5,21 @@ import java.util.Date
 import java.util.Locale
 
 object DateUtils {
-    fun formatTimestampDate(timestamp: Long): String {
-        val format = TIMESTAMP_FORMAT_DATE
+    fun formatTimestampDate(
+        timestamp: Long,
+        pattern: String = TIMESTAMP_FORMAT_DATE_DISPLAY) : String {
         val sdf = SimpleDateFormat(
-            format,
+            pattern,
             Locale.getDefault())
         return sdf.format(Date(timestamp))
     }
 
     fun getTodayDate(): String {
         return formatTimestampDate(
-            System.currentTimeMillis())
+            System.currentTimeMillis(),
+            TIMESTAMP_FORMAT_DATE_STORAGE)
     }
 
-    private const val TIMESTAMP_FORMAT_DATE = "MMM dd, yyyy"
+    private const val TIMESTAMP_FORMAT_DATE_DISPLAY = "MMM dd, yyyy"
+    private const val TIMESTAMP_FORMAT_DATE_STORAGE = "yyyy-MM-dd"
 }
